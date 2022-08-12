c64cae74ebcc6ab568b4289341acd4872e8afa41
admin <admin@MacBook-Pro.local>
2022-08-12 22:45:18 +0300

    add_views

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from orders.models import SalesOrder
+from orders.serializers import OrderSerializers
 
 
 def orders_page(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+
+
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializers
+
+
+def orders_app(request):
+    return render(request, 'main_app.html')
